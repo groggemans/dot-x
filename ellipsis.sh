ff0fe8b1467a36758f4a04fbd1f71a0a4d8a8104
G. Roggemans <g.roggemans@grog.be>
2017-02-25 20:09:49 +0100

    Use improved update strategy

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,7 +7,7 @@
 ##############################################################################
 
 # Minimal ellipsis version
-ELLIPSIS_VERSION_DEP='1.9.0'
+ELLIPSIS_VERSION_DEP='1.9.4'
 
 # Package dependencies (informational/not used!)
 ELLIPSIS_PKG_DEPS='ellipsis/ellipsis-compiler'
@@ -52,12 +52,17 @@ pkg.init() {
 
 ##############################################################################
 
-pkg.pull(){
-    # Update dot-x repo
-    git.pull
+pkg.pull() {
+    # Use improved update strategy
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+        pkg.unlink
+        git.pull
+        pkg.link
 
-    # Update the config files
-    compile_config
+        # Update the config files
+        compile_config
+    fi
 }
 
 ##############################################################################
